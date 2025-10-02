08114e887a9d8dac64771d1c970c9adcb861703f
loppocalypse <erenaslan1782@gmail.com>
2025-10-02 17:21:16 +0200

    Optimisation

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -2,26 +2,10 @@ import Image from "next/image";
 import Header from "@/app/_components/header";
 import AboutUs from "@/app/AboutUs/AboutUs";
 
-{/*
-import { Hero } from "@/app/AboutUs/sections/Hero";
-import { LogoTicker } from "@/app/sections/LogoTicker";
-import { Testimonials } from "@/app/AboutUs/sections/Testimonials";
-import { CallToAction } from "@/app/AboutUs/sections/CallToAction";
-import { Footer } from "@/app/AboutUs/sections/Footer";
-*/}
-
 export default function Home() {
   return (
     <>
       <AboutUs />
-      {/*
-      <Header />
-      <Hero />
-      <LogoTicker />
-      <Testimonials />
-      <CallToAction />
-      <Footer />
-      */}
     </>
   );
 }
